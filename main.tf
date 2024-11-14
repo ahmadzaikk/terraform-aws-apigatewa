718878bdf9a6ca6b958dd8ae52b0c1c55af2689c
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-11-14 15:39:06 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,12 +39,6 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   integration_http_method = "POST"
   type                    = "AWS"
   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${each.value.lambda_function_arn}/invocations"
-
-  # Remove dynamic references
-  depends_on = [
-    aws_api_gateway_method.api_method,  # Static reference to the methods
-    aws_api_gateway_method_response.api_method_response  # Static reference to method responses
-  ]
 }
 
 # Grant API Gateway permission to invoke the respective Lambda function for each resource
@@ -92,12 +86,6 @@ resource "aws_api_gateway_integration_response" "api_integration_response" {
   response_templates = {
     "application/json" = ""
   }
-
-  # Remove dynamic references
-  depends_on = [
-    aws_api_gateway_method_response.api_method_response,  # Static reference to method responses
-    aws_api_gateway_integration.lambda_integration  # Static reference to integrations
-  ]
 }
 
 # Define OPTIONS method for CORS
@@ -117,7 +105,6 @@ resource "aws_api_gateway_integration" "options_integration" {
   rest_api_id             = aws_api_gateway_rest_api.rest_api.id
   resource_id             = aws_api_gateway_resource.api_resource[each.key].id
   http_method             = aws_api_gateway_method.options_method[each.key].http_method
-  # integration_http_method = "POST"  # Dummy method since we don't call a Lambda for OPTIONS
   type                    = "MOCK"  # Use MOCK integration for OPTIONS
   request_templates = {
     "application/json" = <<EOF
@@ -156,14 +143,12 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
   response_parameters = {
     "method.response.header.Access-Control-Allow-Origin"  = "'*'",
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,GET,POST'",
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,GET,POST'"
   }
 
   response_templates = {
     "application/json" = ""
   }
-
-  depends_on = [aws_api_gateway_method_response.options_method_response]
 }
 
 # Create a resource policy for the API Gateway
@@ -199,7 +184,7 @@ resource "aws_api_gateway_rest_api_policy" "rest_api_policy" {
   })
 }
 
-# Deploy the API
+# Deploy the API and ensure it runs last
 resource "aws_api_gateway_deployment" "api_deployment" {
   depends_on = [
     aws_api_gateway_method_response.api_method_response,
@@ -210,26 +195,27 @@ resource "aws_api_gateway_deployment" "api_deployment" {
     aws_api_gateway_resource.api_resource,
     aws_api_gateway_method.api_method,
     aws_api_gateway_method.options_method,
+    aws_api_gateway_stage.api_stage
   ]
 
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
 
-  # Dynamic trigger
+  # Dynamic trigger to ensure redeployment when API resources change
   lifecycle {
     create_before_destroy = true
   }
 
-  # Hash of API resources
+  # Hash of API resources to trigger redeployment
   triggers = {
     deployment_timestamp = timestamp()
-    api_resources_hash = md5(jsonencode(aws_api_gateway_resource.api_resource))
+    api_resources_hash  = md5(jsonencode(aws_api_gateway_resource.api_resource))
   }
 }
 
 # Create a stage for the deployment
 resource "aws_api_gateway_stage" "api_stage" {
   depends_on = [
-    aws_api_gateway_deployment.api_deployment,
+    aws_api_gateway_deployment.api_deployment,  # Ensure stage is created after deployment
   ]
 
   stage_name    = var.stage_name
