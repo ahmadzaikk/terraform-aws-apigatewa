eb211838b042a1c84ffb19e5053402271cc181ad
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-11-14 16:40:35 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -236,7 +236,8 @@ resource "aws_api_gateway_stage" "api_stage" {
   deployment_id = aws_api_gateway_deployment.api_deployment.id
 
   lifecycle {
-    create_before_destroy = true  # Ensures that a new stage is created before destroying the old one
+    #create_before_destroy = true  # Ensures that a new stage is created before destroying the old one
+    ignore_changes = [deployment_id]
   }
 
   depends_on = [aws_api_gateway_deployment.api_deployment]
